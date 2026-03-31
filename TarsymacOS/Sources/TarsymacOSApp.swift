57380c124d159159b1213b22a99bca1944b64f2f
Leddo <felipexy@hotmail.com>
2026-03-31 14:53:57 -0300

    feat: prompt user to move app to Applications when launched from DMG
    Detects if the app is running from /Volumes/ or Downloads, offers to
    copy it to /Applications and relaunch. Prevents issues with running
    from a mounted disk image.

--- TarsymacOS/Sources/TarsymacOSApp.swift
+++ TarsymacOS/Sources/TarsymacOSApp.swift
@@ -70,7 +70,66 @@ struct TarsymacOSApp: App {
 class TarsyAppDelegate: NSObject, NSApplicationDelegate {
     var daemonManager: DaemonManager?
 
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        checkIfRunningFromDMG()
+    }
+
     func applicationWillTerminate(_ notification: Notification) {
         daemonManager?.markOfflineSync()
+    }
+
+    private func checkIfRunningFromDMG() {
+        let appPath = Bundle.main.bundlePath
+        let isFromVolume = appPath.hasPrefix("/Volumes/")
+        let isFromDownloads = appPath.contains("/Downloads/")
+
+        guard isFromVolume || isFromDownloads else { return }
+
+        let appName = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String ?? "Tarsy"
+        let dest = "/Applications/\(appName).app"
+
+        let alert = NSAlert()
+        alert.messageText = "Move to Applications?"
+        alert.informativeText = "\(appName) needs to be in your Applications folder to work properly. Move it now and relaunch?"
+        alert.alertStyle = .informational
+        alert.addButton(withTitle: "Move & Launch")
+        alert.addButton(withTitle: "Quit")
+
+        let response = alert.runModal()
+
+        if response == .alertFirstButtonReturn {
+            moveToApplicationsAndRelaunch(from: appPath, to: dest)
+        } else {
+            NSApp.terminate(nil)
+        }
+    }
+
+    private func moveToApplicationsAndRelaunch(from source: String, to dest: String) {
+        let fm = FileManager.default
+
+        // Remove existing version if present
+        if fm.fileExists(atPath: dest) {
+            try? fm.removeItem(atPath: dest)
+        }
+
+        do {
+            try fm.copyItem(atPath: source, toPath: dest)
+        } catch {
+            let alert = NSAlert()
+            alert.messageText = "Could not move to Applications"
+            alert.informativeText = error.localizedDescription
+            alert.alertStyle = .critical
+            alert.runModal()
+            NSApp.terminate(nil)
+            return
+        }
+
+        // Relaunch from /Applications
+        let task = Process()
+        task.launchPath = "/usr/bin/open"
+        task.arguments = ["-n", dest]
+        try? task.run()
+
+        NSApp.terminate(nil)
     }
 }
