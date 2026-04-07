2e3987bc72f3e11c6ea3f535c0c719bfa21de25d
Leddo <felipexy@hotmail.com>
2026-04-07 04:41:20 -0300

    refactor: remove unused rotateSimulator AppleScript method
    The CGEvent-based rotation shortcuts replaced the old AppleScript approach.

--- TarsymacOS/Sources/Stream/RemoteInputService.swift
+++ TarsymacOS/Sources/Stream/RemoteInputService.swift
@@ -235,33 +235,6 @@ class RemoteInputService {
         }
     }
 
-    private func rotateSimulator(direction: String) {
-        simctlQueue.async {
-            let menuItem = direction == "left" ? "Rotate Left" : "Rotate Right"
-            let script = "tell application \"System Events\" to tell process \"Simulator\" to click menu item \"\(menuItem)\" of menu \"Device\" of menu bar 1"
-            let process = Process()
-            process.executableURL = URL(fileURLWithPath: "/usr/bin/osascript")
-            process.arguments = ["-e", script]
-            process.standardOutput = Pipe()
-            process.standardError = Pipe()
-            do {
-                try process.run()
-                process.waitUntilExit()
-                if process.terminationStatus != 0 {
-                    let errData = (process.standardError as? Pipe)?.fileHandleForReading.readDataToEndOfFile()
-                    let errStr = errData.flatMap { String(data: $0, encoding: .utf8) } ?? ""
-                    #if DEBUG
-                    print("[RemoteInput] Rotate failed: \(errStr)")
-                    #endif
-                }
-            } catch {
-                #if DEBUG
-                print("[RemoteInput] Rotate error: \(error)")
-                #endif
-            }
-        }
-    }
-
     // MARK: - Keyboard
 
     func typeText(_ text: String) {
